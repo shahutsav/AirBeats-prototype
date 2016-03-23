fdd35f8fb1626a6adc6add0e4d758f7ae7b3eae8
Yebai Zhao <zhaoyebai@gmail.com>
2016-03-23 17:37:22 -0400

    Prototype connection 0.1
    The first test version in 3/12/16

---
+++ AirDrunk/AirDrunk.ino
@@ -0,0 +1,266 @@
+/*
+Pin uesage list
+#  use
+2   Serial to 1053
+9   Reset 1053
+A0  Tone changing
+
+*/
+#include "mpr121.h"
+#include <Wire.h>
+#include <SoftwareSerial.h>
+// define the pins used
+#define VS1053_RX  2 // This is the pin that connects to the RX pin on VS1053
+#define VS1053_RESET 9 // This is the pin that connects to the RESET pin on VS1053
+
+#define VS1053_BANK_DEFAULT 0x00
+#define VS1053_BANK_DRUMS1 0x78
+#define VS1053_BANK_DRUMS2 0x7F
+#define VS1053_BANK_MELODY 0x79
+
+#define MIDI_NOTE_ON  0x90
+#define MIDI_NOTE_OFF 0x80
+#define MIDI_CHAN_MSG 0xB0
+#define MIDI_CHAN_BANK 0x00
+#define MIDI_CHAN_VOLUME 0x07
+#define MIDI_CHAN_PROGRAM 0xC0
+
+int analogPin=A0;
+int a0Input=0;
+int midiInstr=1;
+
+SoftwareSerial VS1053_MIDI(0, 2); 
+
+int irqpin = 3;  // Digital 3 
+boolean touchStates[12]; //to keep track of the 12 previous touch states
+
+//////////////////////////////////////////////////////////////////////SETUP
+void setup() {
+	Serial.begin(9600);
+	Serial.println("VS1053+MPR121 MIDI test");
+	pinMode(irqpin, INPUT);
+	pinMode(VS1053_RESET, OUTPUT);
+	digitalWrite(irqpin, HIGH); //enable pullup resistor
+
+	digitalWrite(VS1053_RESET, LOW);
+  	delay(10);
+  	digitalWrite(VS1053_RESET, HIGH);
+  	delay(10);//reset the VS1053
+
+  	Wire.begin();
+  	mpr121_setup();
+
+  	VS1053_MIDI.begin(31250); // MIDI uses a 'strange baud rate'
+  	midiSetChannelBank(0, VS1053_BANK_MELODY);
+  	midiSetInstrument(0, midiInstr);
+  	midiSetChannelVolume(0, 63);
+}
+///////////////////////////////////////////////////////////////////////LOOP
+void loop() {  
+	readTouchInputs();
+  	/*for (uint8_t i=60; i<69; i++) { //i is the note
+    	midiNoteOn(0, i, 127);
+    	delay(100);
+    	midiNoteOff(0, i, 127);
+  	}*/
+   	readResistorInputs();
+}
+
+
+////////////////////////////////////////////////////////////////////FUNCTIONS
+void readResistorInputs(){
+  	a0Input=analogRead(analogPin)/8;
+  	
+  	if(midiInstr != a0Input;){
+  		if (a0Input>0 && a0Input<129){
+    		midiSetInstrument(0, a0Input);
+    		Serial.println(" current instrument"+String(a0Input));
+    		midiInstr = a0Input;
+  		}else{
+  			Serial.println("instrument selection wrong");
+  		}
+	}
+}
+
+
+  
+
+void midiSetInstrument(uint8_t chan, uint8_t inst) {
+  if (chan > 15) return;
+  inst --; // page 32 has instruments starting with 1 not 0 :(
+  if (inst > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_PROGRAM | chan);  
+  VS1053_MIDI.write(inst);
+}
+
+
+void midiSetChannelVolume(uint8_t chan, uint8_t vol) {
+  if (chan > 15) return;
+  if (vol > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write(MIDI_CHAN_VOLUME);
+  VS1053_MIDI.write(vol);
+}
+
+
+void midiSetChannelBank(uint8_t chan, uint8_t bank) {
+  if (chan > 15) return;
+  if (bank > 127) return;
+  
+  VS1053_MIDI.write(MIDI_CHAN_MSG | chan);
+  VS1053_MIDI.write((uint8_t)MIDI_CHAN_BANK);
+  VS1053_MIDI.write(bank);
+}
+
+
+void midiNoteOn(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+  
+  VS1053_MIDI.write(MIDI_NOTE_ON | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
+
+void midiNoteOff(uint8_t chan, uint8_t n, uint8_t vel) {
+  if (chan > 15) return;
+  if (n > 127) return;
+  if (vel > 127) return;
+  
+  VS1053_MIDI.write(MIDI_NOTE_OFF | chan);
+  VS1053_MIDI.write(n);
+  VS1053_MIDI.write(vel);
+}
+
+
+void mpr121_setup(void){
+
+  set_register(0x5A, ELE_CFG, 0x00); 
+  
+  // Section A - Controls filtering when data is > baseline.
+  set_register(0x5A, MHD_R, 0x01);
+  set_register(0x5A, NHD_R, 0x01);
+  set_register(0x5A, NCL_R, 0x00);
+  set_register(0x5A, FDL_R, 0x00);
+
+  // Section B - Controls filtering when data is < baseline.
+  set_register(0x5A, MHD_F, 0x01);
+  set_register(0x5A, NHD_F, 0x01);
+  set_register(0x5A, NCL_F, 0xFF);
+  set_register(0x5A, FDL_F, 0x02);
+  
+  // Section C - Sets touch and release thresholds for each electrode
+  set_register(0x5A, ELE0_T, TOU_THRESH);
+  set_register(0x5A, ELE0_R, REL_THRESH);
+ 
+  set_register(0x5A, ELE1_T, TOU_THRESH);
+  set_register(0x5A, ELE1_R, REL_THRESH);
+  
+  set_register(0x5A, ELE2_T, TOU_THRESH);
+  set_register(0x5A, ELE2_R, REL_THRESH);
+  
+  set_register(0x5A, ELE3_T, TOU_THRESH);
+  set_register(0x5A, ELE3_R, REL_THRESH);
+  
+  set_register(0x5A, ELE4_T, TOU_THRESH);
+  set_register(0x5A, ELE4_R, REL_THRESH);
+  
+  set_register(0x5A, ELE5_T, TOU_THRESH);
+  set_register(0x5A, ELE5_R, REL_THRESH);
+  
+  set_register(0x5A, ELE6_T, TOU_THRESH);
+  set_register(0x5A, ELE6_R, REL_THRESH);
+  
+  set_register(0x5A, ELE7_T, TOU_THRESH);
+  set_register(0x5A, ELE7_R, REL_THRESH);
+  
+  set_register(0x5A, ELE8_T, TOU_THRESH);
+  set_register(0x5A, ELE8_R, REL_THRESH);
+  
+  set_register(0x5A, ELE9_T, TOU_THRESH);
+  set_register(0x5A, ELE9_R, REL_THRESH);
+  
+  set_register(0x5A, ELE10_T, TOU_THRESH);
+  set_register(0x5A, ELE10_R, REL_THRESH);
+  
+  set_register(0x5A, ELE11_T, TOU_THRESH);
+  set_register(0x5A, ELE11_R, REL_THRESH);
+  
+  // Section D
+  // Set the Filter Configuration
+  // Set ESI2
+  set_register(0x5A, FIL_CFG, 0x04);
+  
+  // Section E
+  // Electrode Configuration
+  // Set ELE_CFG to 0x00 to return to standby mode
+  set_register(0x5A, ELE_CFG, 0x0C);  // Enables all 12 Electrodes
+  
+  
+  // Section F
+  // Enable Auto Config and auto Reconfig
+  set_register(0x5A, ATO_CFG0, 0x0B);
+  set_register(0x5A, ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   set_register(0x5A, ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
+  set_register(0x5A, ATO_CFGT, 0xB5);  // Target = 0.9*USL = 0xB5 @3.3V
+  
+  set_register(0x5A, ELE_CFG, 0x0C);
+  
+}
+
+void readTouchInputs(){
+  if(!checkInterrupt()){
+    
+    //read the touch state from the MPR121
+    Wire.requestFrom(0x5A,2); 
+    
+    byte LSB = Wire.read();
+    byte MSB = Wire.read();
+    
+    uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
+
+    
+    for (int i=0; i < 12; i++){  // Check what electrodes were pressed
+      if(touched & (1<<i)){
+      
+        if(touchStates[i] == 0){
+          //pin i was just touched
+          	Serial.print("pin ");
+          	Serial.print(i);
+          	Serial.println(" was just touched");
+        	midiNoteOn(0, i+59, 127);
+        }else if(touchStates[i] == 1){
+          //pin i is still being touched
+        }  
+      
+        touchStates[i] = 1;      
+      }else{
+        if(touchStates[i] == 1){
+          Serial.print("pin ");
+          Serial.print(i);
+          Serial.println(" is no longer being touched");
+          midiNoteOff(0, i+59, 127);
+          //pin i is no longer being touched
+       }
+        
+        touchStates[i] = 0;
+      }
+    
+    }
+    
+  }
+}
+
+boolean checkInterrupt(void){
+  return digitalRead(irqpin);
+}
+
+
+void set_register(int address, unsigned char r, unsigned char v){
+    Wire.beginTransmission(address);
+    Wire.write(r);
+    Wire.write(v);
+    Wire.endTransmission();
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# HCIN 722 Group Poject AirBeats
+The is a repository for code and files used in the project AirBeasts.
+The AirBeast is approching to help people making music on the go.
+
